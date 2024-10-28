8252d27379a7e829f02c084f5e12ccb52e2d1d14
Bami O. <bamideleogundeji949@gmail.com>
2024-10-28 14:20:14 -0400

    first commit

---
+++ game.js
@@ -0,0 +1,8 @@
+//Variables
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+function nextSequence(){
+    //make a random number between 0 and 3
+    randomNumber = Math.floor(Math.random() * 4);
+}
+
