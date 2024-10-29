6e3169e9987d375baacfebb287a39e41c0598a98
Bami O. <bamideleogundeji949@gmail.com>
2024-10-29 16:14:57 -0400

    add animations and sound when nextSequence() is triggered

--- game.js
+++ game.js
@@ -2,6 +2,11 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 
+function playSound(color){
+    var audio = new Audio("sounds/"+ color +".mp3");
+    audio.play();
+}
+
 
 function nextSequence(){
     //make a random number between 0 and 3, choosing said color, and adding it to the gamePattern array
@@ -9,6 +14,9 @@ function nextSequence(){
     var randomChosenColor = buttonColours[randomNumber];
 
     gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColor);
 }
 
 //light up the button when mouse is hovered over it
@@ -24,4 +32,10 @@ for(var i = 0; i < buttonColours.length; i++){
 );
 }
 
- $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+$("#blue").fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+
+
+
+
+ 
